0f594e5350907260dc478f285dddb90ce37d19a6
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2026-03-14 18:20:37 +0800

    ci

---
+++ scripts/ci.py
@@ -0,0 +1,12 @@
+import os, subprocess
+import sys
+os.makedirs('collect')
+
+for _d, _, _fs in os.walk("."):
+    for _f in _fs:
+        if not _f.endswith(".whl"):
+            continue
+        _f=os.path.join(_d, _f)
+        os.rename(_f, f'collect/{_f}')
+os.chdir('collect')
+subprocess.run(f'twine upload -u __token__ -p {sys.argv[2]} ./*.whl')
